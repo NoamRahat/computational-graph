ff7431355bbad0b61732daa73373ebae675488ae
Noam Rahat <noamrht@gmail.com>
2024-05-28 21:37:54 +0300

    Topic Class
    `subs` is a list of agents that are subscribed to the topic, and `pubs` is a list of agents that can publish messages to the topic. The subscribe(), unsubscribe(), addPublisher(), and removePublisher() methods modify these lists as appropriate. The publish() method sends the given message to all subscribed agents by calling their callback() method.
    
    Signed-off-by: Noam Rahat <noamrht@gmail.com>

--- src/test/Topic.java
+++ src/test/Topic.java
@@ -1,27 +1,38 @@
-package project_biu.graph;
+package test;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class Topic {
     public final String name;
+    private final List<Agent> subs;
+    private final List<Agent> pubs;
+
     Topic(String name){
-        this.name=name;
+        this.name = name;
+        this.subs = new ArrayList<>();
+        this.pubs = new ArrayList<>();
     }
 
     public void subscribe(Agent a){
+        subs.add(a);
     }
+
     public void unsubscribe(Agent a){
+        subs.remove(a);
     }
 
     public void publish(Message m){
+        for (Agent a : subs) {
+            a.callback(name, m);
+        }
     }
 
     public void addPublisher(Agent a){
+        pubs.add(a);
     }
 
     public void removePublisher(Agent a){
+        pubs.remove(a);
     }
-
-
 }
