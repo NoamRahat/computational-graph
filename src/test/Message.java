d6b1b2029c8124ceb5b4a3f27bf5f9f5402a66ba
Noam Rahat <noamrht@gmail.com>
2024-05-28 21:24:45 +0300

    Message Class
    Define 3 public constructors that take a byte array, a string, or a double as input. Each of these constructors calls a private constructor that takes all four fields as parameters.The private constructor is the one that actually initializes the fields.
    
    parseDouble() method is a helper method that tries to convert a string to a double. If the conversion fails, it returns Double.NaN as specified.
    
    Signed-off-by: Noam Rahat <noamrht@gmail.com>

--- src/test/Message.java
+++ src/test/Message.java
@@ -8,4 +8,31 @@ public class Message {
     public final String asText;
     public final double asDouble;
     public final Date date;
+
+    public Message(byte[] data) {
+        this(data, new String(data, StandardCharsets.UTF_8), parseDouble(new String(data, StandardCharsets.UTF_8)), new Date());
+    }
+
+    public Message(String asText) {
+        this(asText.getBytes(StandardCharsets.UTF_8), asText, parseDouble(asText), new Date());
+    }
+
+    public Message(double asDouble) {
+        this(String.valueOf(asDouble).getBytes(StandardCharsets.UTF_8), String.valueOf(asDouble), asDouble, new Date());
+    }
+
+    private Message(byte[] data, String asText, double asDouble, Date date) {
+        this.data = data;
+        this.asText = asText;
+        this.asDouble = asDouble;
+        this.date = date;
+    }
+
+    private static double parseDouble(String s) {
+        try {
+            return Double.parseDouble(s);
+        } catch (NumberFormatException e) {
+            return Double.NaN;
+        }
+    }
 }
